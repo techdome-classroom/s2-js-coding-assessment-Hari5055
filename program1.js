7350a1b98a470c66c3373b6649cd48defd4946cd
Hari Gandham <harigandham@Haris-MacBook-Air.local>
2024-11-11 15:54:54 +0530

    initial commit

--- program1.js
+++ program1.js
@@ -3,6 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char in mapping) {
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            if (mapping[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
     
 };
 
